278a81b335a571c153a771dd38eda4faf5b05373
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-02-28 10:29:23 +0000

    Add main application routing with authentication and home page routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import ResetPassword from "./pages/auth/ResetPassword";
+
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
