aaabdf554b8cbf0b65c1b5195e80b5417a398636
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-02-28 16:25:27 +0000

    Add project and sample management routes with user authentication

---
+++ backend/routes/project/projectRoutes.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+const { verifyToken } = require("../../middleware/authMiddleware");
+const { isChercheur } = require("../../middleware/roleMiddleware");
+const projectController = require("../../controllers/projectController");
+const userController = require("../../controllers/userController");
+
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+// get users and user by id
+router.get(
+  "/available-team-members",
+  verifyToken,
+  userController.getAvailableTeamMembers
+);
+
+router.get(
+  "/available-technicians",
+  verifyToken,
+  userController.getAvailableTechnicians
+);
+
+// Project routes
+router.post(
+  "/project",
+  verifyToken,
+  isChercheur,
+  projectController.createProject
+);
+
+// Sample routes
+router.post(
+  "/projects/:projectId/samples",
+  verifyToken,
+  isChercheur,
+  upload.single("protocolFile"),
+  projectController.addSampleToProject
+);
+
+router.get(
+  "/projects/:projectId/samples",
+  verifyToken,
+  projectController.getSamplesByProject
+);
+
+// Get all projects
+
+module.exports = router;
