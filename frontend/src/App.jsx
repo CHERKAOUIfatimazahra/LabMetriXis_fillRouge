a640aecd070110fbfc8e80c71ef102af953abd3a
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-02-28 15:32:36 +0000

    Add routes for ResendOTP, OTP, and ResearcherDashboard components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,11 @@ import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import ResetPassword from "./pages/auth/ResetPassword";
+import ResendOTP from "./pages/auth/ResendOTP";
+import OTP from "./pages/auth/OTP";
+
+// dashboard researcher
+import ResearcherDashboard from "./pages/dashboard/researcher/ResearcherDashboard";
 
 import HomePage from "./pages/HomePage";
 
@@ -11,10 +16,22 @@ function App() {
     <Router>
       <Routes>
         <Route path="/home" element={<HomePage />} />
+
+        {/* authentification */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/resend-otp" element={<ResendOTP />} />
+        <Route path="/otp" element={<OTP />} />
+
+        {/* dashboard */}
+
+        {/* researcher dashboard */}
+        <Route path="/dashboard/researcher" element={<ResearcherDashboard />} />
+
+        {/* technicien dashboard */}
+
         <Route path="/" element={<Login />} />
       </Routes>
     </Router>
