04b870d677efd695eae695cf294b90e8b26b97c7
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-03-14 22:18:11 +0000

    feat: add sample routes for retrieving all samples by technician ID

---
+++ backend/routes/samples/samplesRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const { verifyToken } = require("../../middleware/authMiddleware");
+const sampleController = require("../../controllers/sampleController");
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname); 
+  },
+});
+const upload = multer({ storage: storage });
+
+
+// Sample routes
+
+// get all samples by id of the technician
+router.get(
+  "/samples",
+  verifyToken,
+  sampleController.getAllSamples
+);
+
+module.exports = router;
